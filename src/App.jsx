337600350c9a7f0664ae8f14f106701d0fdf75de
Alexis Rabbia <alexisrabbia@gmail.com>
2024-04-02 20:14:50 -0300

    Ejercicio filtrar apellidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 
 import './App.css'
-import Family from './components/family/Family';
+import Persons from './components/persons/Persons';
 
 function App() {
-    const persons = [{ name: 'Juan', age: 26 }, { name: 'Gabriel', age: 27 }, { name: 'Valentina', age: 22 }, { name: 'Paula', age: 25 }, { name: 'Andrés', age: 20 }];
-
-    const personsOrder = persons.sort((a, b) => b.age - a.age)
-
+    const names = ['Parker','Simmons','Lewis','Poe'];
+    const nameFilters = names.filter((nameF) => {return nameF.charAt(0) === 'P'})
     return (
-        <div>
+        <div>            
             <h1>Lista de nombres:</h1>
-            <Family persons={personsOrder} />
+            {nameFilters.map((name, index) => (
+                <Persons key={index} name={name} />
+            ))}
         </div>
     );
 }
