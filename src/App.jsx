1282d0f16c7e8b795e1a969b9b326f56aabc50ff
Alexis Rabbia <alexisrabbia@gmail.com>
2024-04-03 18:36:15 -0300

    arreglo url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import BookItem from "./components/bookItem/BookItem.jsx";
+import BookItem from './components/bookItem/Bookitem'
 
 const books = [
   {
@@ -43,24 +43,30 @@ const App = () => {
         author={books[0].bookAuthor}
         rating={books[0].bookRating}
         pageCount={books[0].pageCount}
+        imageUrl={books[0].imageUrl}
       />
       <BookItem
         title={books[1].bookTitle}
         author={books[1].bookAuthor}
         rating={books[1].bookRating}
         pageCount={books[1].pageCount}
+        imageUrl={books[1].imageUrl}
       />
       <BookItem
         title={books[2].bookTitle}
         author={books[2].bookAuthor}
         rating={books[2].bookRating}
         pageCount={books[2].pageCount}
+        
+        imageUrl={books[2].imageUrl}
       />
       <BookItem
         title={books[3].bookTitle}
         author={books[3].bookAuthor}
         rating={books[3].bookRating}
         pageCount={books[3].pageCount}
+        
+        imageUrl={books[3].imageUrl}
       />
 
     </div>
